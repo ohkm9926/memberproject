611f86d07b975b9fc32a1c04535de16150c914bb
ohkyungmin <ohkm9926@naver.com>
2024-06-28 11:21:14 +0900

    second commit

--- src/main/java/com/member/member/controller/MemberController.java
+++ src/main/java/com/member/member/controller/MemberController.java
@@ -1,14 +1,15 @@
 package com.member.member.controller;
 
+
 import com.member.member.dto.MemberDTO;
 import com.member.member.service.MemberService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -45,5 +46,55 @@ public class MemberController {
                }
 
         }
+    @GetMapping("/member/list")
+    public String findAll(Model model){
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        model.addAttribute("memberList" , memberDTOList);
+        return "list";
+
+    }
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id , Model model){
+        MemberDTO  memberDTO = memberService.findById(id);
+        model.addAttribute("member" ,memberDTO);
+        return "detail";
+
+
+
+    }
+    @GetMapping("/member/update")
+     public String updateForm(HttpSession session, Model model){
+        String myEmail  =(String) session.getAttribute("loginEmail");
+        MemberDTO memberDTO = memberService.updateForm(myEmail);
+        model.addAttribute("updateMember" , memberDTO);
+        return "update";
+    }
+    @PostMapping("/member/update")
+    public String update(@ModelAttribute MemberDTO memberDTO){
+        memberService.update(memberDTO);
+        return "redirect:/member/"+memberDTO.getId();
+    }
+    @GetMapping("/member/delete/{id}")
+    public String delete(@PathVariable Long id){
+        memberService.deleteById(id);
+        return  "redirect:/member/list";
+    }
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "index";
+    }
+    @PostMapping("/member/email-check")
+    public @ResponseBody String emailCheck(@RequestParam("memberEmail")String memberEmail){
+        System.out.println("memberEmail =" + memberEmail);
+        String checkResult = memberService.emailCheck(memberEmail);
+        return checkResult;
+//        if (checkResult != null){
+//            return "ok";
+//        }else {
+//            return "no";
+//        }
+
+    }
 
 }
--- src/main/java/com/member/member/service/MemberService.java
+++ src/main/java/com/member/member/service/MemberService.java
@@ -6,6 +6,8 @@ import com.member.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -43,6 +45,56 @@ public class MemberService {
         } else {
             //조회결과가 없다(해당 이메일을 가진 회원이 없다.)
             return null;
+        }
+
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for (MemberEntity memberEntity : memberEntityList){
+            memberDTOList.add(MemberDTO.toMemberDTO(memberEntity));
+
+        }
+        return memberDTOList;
+    }
+
+    public MemberDTO findById(Long id) {
+        Optional<MemberEntity> optionlMemberEntity = memberRepository.findById(id);
+        if (optionlMemberEntity.isPresent()){
+            return MemberDTO.toMemberDTO(optionlMemberEntity.get());
+        }else{
+            return null;
+        }
+
+    }
+
+    public MemberDTO updateForm(String myEmail) {
+        Optional<MemberEntity> optionalMemberEntity= memberRepository.findByMemberEmail(myEmail);
+        if (optionalMemberEntity.isPresent()){
+            return MemberDTO.toMemberDTO(optionalMemberEntity.get());
+        }else {
+            return null;
+        }
+
+    }
+
+    public void update(MemberDTO memberDTO) {
+        memberRepository.save(MemberEntity.toUpdateMemberEntity(memberDTO));
+
+    }
+
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public String emailCheck(String memberEmail) {
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberEmail);
+        if(byMemberEmail.isPresent()){
+            //조회결과이 있다는건 사용할수 없다
+            return null;
+        }else{
+            return "ok";
         }
 
     }
